c0947e15ec648303895e98aa1c185bbc9f51e712
ytgw <tagawa0920@gmail.com>
2022-05-01 22:12:27 +0900

    ToDoリストのモデルを追加。

--- todo_list/models.py
+++ todo_list/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class ToDoList(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    todo_list = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
