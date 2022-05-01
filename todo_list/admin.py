1bd635117ff1391d949102dfb532d0b487719fc1
ytgw <tagawa0920@gmail.com>
2022-05-01 22:29:00 +0900

    管理ページをカスタマイズ。

--- todo_list/admin.py
+++ todo_list/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import User, ToDoList, Task
 
-# Register your models here.
+
+class ToDoListInline(admin.TabularInline):
+    model = ToDoList
+    extra = 2
+
+class UserAdmin(admin.ModelAdmin):
+    inilines = [ToDoListInline]
+
+
+class TaskInline(admin.TabularInline):
+    model = Task
+    extra = 2
+
+class ToDoListAdmin(admin.ModelAdmin):
+    inlines = [TaskInline]
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(ToDoList, ToDoListAdmin)
