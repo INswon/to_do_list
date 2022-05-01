03a4c6fd5e4624433d6e41af29694886ad6db0b4
ytgw <tagawa0920@gmail.com>
2022-05-01 23:04:01 +0900

    要件変更に合わせてモデル関連を修正。

--- todo_list/admin.py
+++ todo_list/admin.py
@@ -1,22 +1,13 @@
 from django.contrib import admin
-from .models import User, ToDoList, Task
-
-
-class ToDoListInline(admin.TabularInline):
-    model = ToDoList
-    extra = 2
-
-class UserAdmin(admin.ModelAdmin):
-    inilines = [ToDoListInline]
+from .models import User, Task
 
 
 class TaskInline(admin.TabularInline):
     model = Task
     extra = 2
 
-class ToDoListAdmin(admin.ModelAdmin):
+class UserAdmin(admin.ModelAdmin):
     inlines = [TaskInline]
 
 
 admin.site.register(User, UserAdmin)
-admin.site.register(ToDoList, ToDoListAdmin)
--- todo_list/models.py
+++ todo_list/models.py
@@ -8,17 +8,9 @@ class User(models.Model):
         return self.name
 
 
-class ToDoList(models.Model):
+class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255, unique=True)
-
-    def __str__(self):
-        return self.name
-
-
-class Task(models.Model):
-    todo_list = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
