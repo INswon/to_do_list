f2eb0dac90fb3421552b2901dc88d5aef1dd7e48
ytgw <tagawa0920@gmail.com>
2022-05-02 16:30:43 +0900

    todo_listアプリ全体のurlを変更。

--- auth_sample/urls.py
+++ auth_sample/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('todo_list/', include('todo_list.urls')),
+    path('', include('todo_list.urls')),
     path('admin/', admin.site.urls),
 ]
