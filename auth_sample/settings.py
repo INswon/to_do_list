f086b9a781fd06ed4b42184fcf1a67084bfd832e
ytgw <tagawa0920@gmail.com>
2022-05-02 22:43:03 +0900

    setting.pyを更新。

--- auth_sample/settings.py
+++ auth_sample/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8ps#q9c^wec@r4l^i17jb4$u&o@9t%0)k9)c5pwf4cwq-#!nvr'
+from .secret import SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
